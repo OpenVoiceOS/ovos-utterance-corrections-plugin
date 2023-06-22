11734d36de6fec82ec32197b7ec9ee3291f729ca
JarbasAi <jarbasai@mailfence.com>
2023-06-22 01:42:40 +0100

    fuzzy match

--- ovos_utterance_corrections_transformer/__init__.py
+++ ovos_utterance_corrections_transformer/__init__.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 from json_database import JsonStorage
 from ovos_config.meta import get_xdg_base
 from ovos_plugin_manager.templates.transformers import UtteranceTransformer
+from ovos_utils.parse import match_one
 from ovos_utils.xdg_utils import xdg_data_home
 
 
@@ -12,10 +13,10 @@ class UtteranceCorrectionsPlugin(UtteranceTransformer):
         super().__init__(name, priority)
         self.db = JsonStorage(path=f"{xdg_data_home()}/{get_xdg_base()}/corrections.json")
 
-    def transform(self, utterances: List[str],
-                  context: Optional[dict] = None) -> (list, dict):
+    def transform(self, utterances: List[str], context: Optional[dict] = None) -> (list, dict):
         context = context or {}
-        for u in utterances:
-            if u in self.db:
-                return [self.db[u]], context
+        replacement, conf = match_one(utterances[0], self.db)  # TODO - match strategy from conf
+        if conf >= 0.8:  # TODO make configurable
+            return [replacement], context
         return utterances, context
+
