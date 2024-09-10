af7645c2a560b3e6556f99a403f01b61ef5c1428
miro <jarbasai@mailfence.com>
2024-09-10 23:05:36 +0100

    chore:automations

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 def get_version():
     """ Find the version of the package"""
-    version = None
-    version_file = os.path.join(BASEDIR, 'ovos_utterance_corrections_transformer', 'version.py')
+    version_file = f'{BASEDIR}/ovos_utterance_corrections_transformer/version.py'
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
